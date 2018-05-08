097656c991b0bfac1cc73a4461fafe9322adc2e3
OUALIomar <oualiomar22@yahoo.fr>
2018-05-08 20:44:49 +0200

    Ajout des deux classes d'exception

---
+++ grapheorienté/ArcInexistantException.java
@@ -0,0 +1,9 @@
+package grapheorienté;
+
+public class ArcInexistantException extends Exception {
+	
+	public ArcInexistantException() {}
+	public ArcInexistantException(int o, int e) {
+		super("L'arc reliant le sommet " + o + " au sommet " + e + " n'est pas défini.");
+	}
+}
---
+++ grapheorienté/SommetInexistantException.java
@@ -0,0 +1,10 @@
+package grapheorienté;
+
+public class SommetInexistantException extends Exception {
+	
+	public SommetInexistantException() {}
+	public SommetInexistantException(int i,String etiq) {
+		super("Le sommet " + i + " n'est pas défini. ");
+	}
+
+}
